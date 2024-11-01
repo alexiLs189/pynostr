a74e62d13c75f3be22998937e4a149db3a5b4624
alexiLs189 <139680659+alexiLs189@users.noreply.github.com>
2024-11-01 11:16:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    name='pynostr',
     use_scm_version={'write_to': 'pynostr/_version.py'},
     setup_requires=['setuptools_scm'],
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='alexiLs189',
+    description='pynostr with async publishing',
+    url='https://github.com/alexiLs189/pynostr.git', 
+    python_requires='>=3.6',
 )
+
