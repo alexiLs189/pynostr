6af9fad86d559e74812068bb9c9f531ffacc1743
alexiLs189 <139680659+alexiLs189@users.noreply.github.com>
2024-11-01 11:33:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pynostr_v2',
+    name='pynostrV2',
     use_scm_version={'write_to': 'pynostr/_version.py'},
     setup_requires=['setuptools_scm'],
     packages=find_packages(),
